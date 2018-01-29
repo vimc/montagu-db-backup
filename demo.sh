85de95dc401055dc46d20cef131a7a31d605aa5d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-01-29 15:28:16 +0000

    Tidy up demo script

--- demo.sh
+++ demo.sh
@@ -8,7 +8,6 @@ docker network create pg_nw
 docker volume create pg_server_data
 docker volume create barman_data
 docker volume create barman_restore
-docker volume create barman_etc
 
 docker run --rm -d \
        --name pg_server \
@@ -34,15 +33,15 @@ docker exec -it pg_server psql -w -U vimc -d montagu -c 'CREATE TABLE foo (bar I
 docker exec -it pg_server psql -w -U vimc -d montagu -c 'INSERT INTO foo VALUES (1);'
 
 docker run -d --rm \
-       --name barman \
+       --name barman_container \
        --network pg_nw \
        -v barman_data:/var/lib/barman \
        -v barman_restore:/restore \
        docker.montagu.dide.ic.ac.uk:5000/montagu-barman:i1333
 
-docker exec barman setup-barman
-docker exec barman barman list-backup all
-docker exec barman restore-last
+docker exec barman_container setup-barman
+docker exec barman_container barman list-backup all
+docker exec barman_container restore-last
 
 ## Then try and use the instance:
 docker run --rm -d \
