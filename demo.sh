e826a114a9da3edbc6c58e37c78c6990dc677747
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-02-05 16:17:03 +0000

    Better default values for postgres server

--- demo.sh
+++ demo.sh
@@ -3,32 +3,32 @@
 ./teamcity-build.sh
 
 docker network create pg_nw
-docker volume create pg_server_data
+docker volume create db_data
 docker volume create barman_data
 docker volume create barman_restore
 
 docker run --rm -d \
-       --name pg_server \
+       --name db \
        --network pg_nw \
        -p 5435:5432 \
-       -v pg_server_data:/pgdata \
+       -v db_data:/pgdata \
        docker.montagu.dide.ic.ac.uk:5000/montagu-db:i1333
-docker exec pg_server montagu-wait.sh
+docker exec db montagu-wait.sh
 
 ## This will fit pretty happily into the general deployment approach
 ## that we have.  This, in addition to setting barman's password, also
 ## creates the replication slot on the server if it is not there
 ## already.
-docker exec pg_server enable-replication.sh changeme
+docker exec db enable-replication.sh changeme changeme
 
 ## Restore the db - this takes *ages* unfortunately, particularly on
-## docker cp db.dump pg_server:/db.dump
-## docker exec pg_server restore-dump.sh /db.dump
+## docker cp db.dump db:/db.dump
+## docker exec db restore-dump.sh /db.dump
 ## docker exec rm /db.dump
 
 ## Or, put at least one transaction worth of data in:
-docker exec -it pg_server psql -w -U vimc -d montagu -c 'CREATE TABLE foo (bar INTEGER)'
-docker exec -it pg_server psql -w -U vimc -d montagu -c 'INSERT INTO foo VALUES (1);'
+docker exec -it db psql -w -U vimc -d montagu -c 'CREATE TABLE foo (bar INTEGER)'
+docker exec -it db psql -w -U vimc -d montagu -c 'INSERT INTO foo VALUES (1);'
 
 docker run -d --rm \
        --name barman_container \
@@ -43,16 +43,17 @@ docker exec barman_container restore-last
 
 ## Then try and use the instance:
 docker run --rm -d \
-       --name pg_server_recovered \
+       --name db_recovered \
        -v barman_restore:/pgdata \
        docker.montagu.dide.ic.ac.uk:5000/montagu-db:i1333
-docker exec pg_server_recovered montagu-wait.sh
-docker exec -it pg_server_recovered \
+docker exec db_recovered montagu-wait.sh
+docker exec -it db_recovered \
        psql -U vimc -d montagu -c \
        "\dt"
 
 ## Again, but without the server running:
-docker stop pg_server_recovered
+docker stop db
+docker stop db_recovered
 docker exec barman_container wipe-restore
 docker stop barman_container
 
@@ -63,11 +64,11 @@ docker run --rm \
        docker.montagu.dide.ic.ac.uk:5000/montagu-barman:i1333
 
 docker run --rm -d \
-       --name pg_server_recovered \
+       --name db_recovered \
        -v barman_restore:/pgdata \
        docker.montagu.dide.ic.ac.uk:5000/montagu-db:i1333
-docker exec pg_server_recovered montagu-wait.sh
-docker exec -it pg_server_recovered \
+docker exec db_recovered montagu-wait.sh
+docker exec -it db_recovered \
        psql -U vimc -d montagu -c \
        "\dt"
-docker stop pg_server_recovered
+docker stop db_recovered
