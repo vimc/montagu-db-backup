fcb784af9b6b0c12f219a3ffe5605797b6bb07f5
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-03-19 12:46:35 +0000

    Set up barman more closely to eventual deployment

--- demo_montagu.sh
+++ demo_montagu.sh
@@ -1,57 +1,55 @@
-## As for demo.sh but assuming that we have montagu running via
-## deploy.
+## Start a montagu
 
-## NOTE: it is not recommended to run this through noninteractively -
-## we'll get a better version of this into the deploy work.  Some of
-## the steps here will take ages!
 
-## This will have created
+## NOTE: We need to know the ip address of the host where the montagu
+## db is running.  This can be done by setting this container to have
+## --network=host and using localhost but that will require a
+## different configuration when used in anger.  With this approach we
+## just set
 ##
-## - a network 'montagu_default'
-## - a container 'montagu_db_1'
-## - postgres on the network has hostname 'db'
+##     MONTAGU_DB_IP=$(host production.montagu.dide.ic.ac.uk)
 ##
-## We will assume all of that here:
-MONTAGU_NETWORK=montagu_default
-MONTAGU_DB_CONTAINER=montagu_db_1
-MONTAGU_DB_NAME=db
+## and the rest of the script remains unchanged.
+MONTAGU_DB_IP=$(hostname --ip-address)
 
-## These will need configuring through the deploy script too
+## This is the only other bit of configuration needed - it will need
+## tweaking if using the 'fake' password group (an if/else here or add
+## fake entries into the vault).
+MONTAGU_PASSWORD_GROUP=production
+
+## Then the bits that are less interesting to configure:
 BARMAN_VOLUME_DATA=barman_data
 BARMAN_VOLUME_RESTORE=barman_restore
 BARMAN_VOLUME_LOGS=barman_logs
 BARMAN_CONTAINER=barman_container
 BARMAN_IMAGE=docker.montagu.dide.ic.ac.uk:5000/montagu-barman:master
 
-## Prep in the postgres container - this should happen during deploy:
-## docker exec $MONTAGU_DB_CONTAINER enable-replication.sh changeme changeme
-
-docker volume create $BARMAN_VOLUME_DATA
-docker volume create $BARMAN_VOLUME_RESTORE
-docker volume create $BARMAN_VOLUME_LOGS
-
-# vault auth -method=github
-# BARMAN_MONTAGU_PASS_BACKUP=$(vault read -field=password secret/database/production/users/barman)
-# BARMAN_MONTAGU_PASS_STREAM=$(vault read -field=password secret/database/production/users/streaming_barman)
-BARMAN_MONTAGU_PASS_BACKUP=barman
-BARMAN_MONTAGU_PASS_STREAM=streaming_barman
+vault auth -method=github
+MONTAGU_PASSWORD_PATH="secret/database/${MONTAGU_PASSWORD_GROUP}/users"
+BARMAN_MONTAGU_PASS_BACKUP=$(vault read -field=password \
+                                   ${MONTAGU_PASSWORD_PATH}/barman)
+BARMAN_MONTAGU_PASS_STREAM=$(vault read -field=password \
+                                   ${MONTAGU_PASSWORD_PATH}/streaming_barman)
 
 BARMAN_CONFIG=$(tempfile)
-
 cat > $BARMAN_CONFIG <<EOF
 BARMAN_MONTAGU_PASS_BACKUP=$BARMAN_MONTAGU_PASS_BACKUP
 BARMAN_MONTAGU_PASS_STREAM=$BARMAN_MONTAGU_PASS_STREAM
 EOF
 
-## NOTE: this only works because:
+docker volume create $BARMAN_VOLUME_DATA
+docker volume create $BARMAN_VOLUME_RESTORE
+docker volume create $BARMAN_VOLUME_LOGS
+
 docker run -d --rm \
        --name $BARMAN_CONTAINER \
-       --network $MONTAGU_NETWORK \
        -v $BARMAN_VOLUME_DATA:/var/lib/barman \
        -v $BARMAN_VOLUME_LOGS:/var/log/barman \
        -v $BARMAN_VOLUME_RESTORE:/restore \
        --env-file $BARMAN_CONFIG \
+       --add-host="db:${MONTAGU_DB_IP}" \
        $BARMAN_IMAGE
+
 rm -f $BARMAN_CONFIG
 
 docker exec $BARMAN_CONTAINER setup-barman
@@ -64,9 +62,12 @@ docker exec $BARMAN_CONTAINER restore-last
 docker run --rm -d \
        --name db_recovered \
        -v $BARMAN_VOLUME_RESTORE:/pgdata \
-       docker.montagu.dide.ic.ac.uk:5000/montagu-db:i1333
+       docker.montagu.dide.ic.ac.uk:5000/montagu-db:master
 
 docker logs --tail 10 db_recovered
 
-docker exec -it db_recovered psql -U vimc -d montagu -c "\dt"
+docker exec db_recovered psql -U vimc -d montagu -c "\dt"
 docker stop db_recovered
+
+docker stop $BARMAN_CONTAINER
+docker volume rm $BARMAN_VOLUME_DATA $BARMAN_VOLUME_LOGS $BARMAN_VOLUME_RESTORE
