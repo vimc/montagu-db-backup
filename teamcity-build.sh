7d7d68d8b89858fd9fb4a94ac99df05429bd0bd1
Martin Eden <martin.s.eden@pm.me>
2018-05-23 14:41:14 +0100

    Make sure to tag the image first

--- teamcity-build.sh
+++ teamcity-build.sh
@@ -19,6 +19,8 @@ docker build --pull \
 docker push $APP_DOCKER_BRANCH_TAG
 docker push $APP_DOCKER_COMMIT_TAG
 
-if $GIT_BRANCH -eq "i1771"; then
-    docker push $PUBLIC_REGISTRY/$NAME:$GIT_BRANCH
+if [[ $GIT_BRANCH -eq "i1771" ]]; then
+    PUBLIC_TAG=$PUBLIC_REGISTRY/$NAME:master
+    docker tag $APP_DOCKER_BRANCH_TAG $PUBLIC_TAG
+    docker push $PUBLIC_TAG
 fi
