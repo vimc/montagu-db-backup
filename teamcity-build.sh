657395ec25f3d7b172df673e5fdc3ba70dc3c3bb
Martin Eden <martin.s.eden@pm.me>
2018-05-23 14:32:35 +0100

    Also push master commits to public repo

--- teamcity-build.sh
+++ teamcity-build.sh
@@ -4,6 +4,7 @@ set -e
 GIT_ID=$(git rev-parse --short=7 HEAD)
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
 REGISTRY=docker.montagu.dide.ic.ac.uk:5000
+PUBLIC_REGISTRY=vimc
 NAME=montagu-barman
 
 APP_DOCKER_TAG=$REGISTRY/$NAME
@@ -17,3 +18,7 @@ docker build --pull \
 
 docker push $APP_DOCKER_BRANCH_TAG
 docker push $APP_DOCKER_COMMIT_TAG
+
+if GIT_BRANCH -eq "master"; then
+    docker push $PUBLIC_REGISTRY/$NAME:$GIT_BRANCH
+fi
