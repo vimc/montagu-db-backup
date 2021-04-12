6b0a41295f96c88374f6a14f6a152739646b1294
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-04-12 12:43:52 +0100

    Update installation path

--- aws/bin/run-machine-metrics.sh
+++ aws/bin/run-machine-metrics.sh
@@ -9,4 +9,5 @@ function finish {
 trap finish EXIT
 
 git clone https://github.com/vimc/machine-metrics.git $WORKDIR
+git -C machine-metrics checkout vimc-4690
 (cd "${WORKDIR}" && sudo -H ./install)
