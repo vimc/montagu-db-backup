59356ea508d21b0b84728d6da4a57663df3d8e8e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-04-12 12:50:00 +0100

    Fix directory name

--- aws/bin/run-machine-metrics.sh
+++ aws/bin/run-machine-metrics.sh
@@ -9,5 +9,5 @@ function finish {
 trap finish EXIT
 
 git clone https://github.com/vimc/machine-metrics.git $WORKDIR
-git -C machine-metrics checkout vimc-4690
+git -C $WORKDIR checkout vimc-4690
 (cd "${WORKDIR}" && sudo -H ./install)
